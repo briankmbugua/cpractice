3f50d9d25ab57ffd471d50ddd742459f69b64846
briankmbugua <briankmbuguak@gmail.com>
2022-10-26 07:29:49 +0300

    more c

--- dynamicmemoryallocation.c
+++ dynamicmemoryallocation.c
@@ -3,13 +3,21 @@
 
 int main(void)
 {
-    int *a = malloc(sizeof(int)*10); //we can treat this like an array
+    printf("Enter size");
+    int size = 0;
+    scanf("%d", &size);
+    int *a = malloc(sizeof(int)*size); //we can treat this like an array
 
-    for(int i=0;i<10;i++) a[i] = 10 - i;
-    for(int i=0;i<10;i++)
+    for(int i=0;i<size;i++) a[i] = size - i;
+    for(int i=0;i<size;i++)
     printf("a[%d] = %d\n", i, a[i]);
     printf("\n");
     printf("a: %p\n", a);
+    a = realloc(a, sizeof(int) * (size+5));
+    for(int i=size;i<(size+5);i++) a[i] = 9;
+    for(int i=0;i<(size+5);i++)
+    printf("a[%d] = %d\n", i, a[i]);
+    printf("\n");
     free(a);
 
 }
