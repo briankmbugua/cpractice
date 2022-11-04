cf7fd1eea6ca31c22c95f72863b66b8397a76c49
briankmbugua <briankmbuguak@gmail.com>
2022-11-05 02:15:53 +0300

    more c

--- arrayinsertion.c
+++ arrayinsertion.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
 int main(){
-    int array[10] = {1,2,3,4,5};
-    
-    printf("%d", array[5]);
+    int a[10] = {1,2,3,4,5};
+    int size = 5;
+    int num,i,pos;
+    printf("enter the number to insert\n");
+    scanf("%d", &num);
+    printf("enter the position to insert at\n");
+    scanf("%d", &pos);
+    for(i = size -1; i>=pos-1;i--){
+        a[i+1] = a[i];
+    }
+    a[pos-1] = num;
+    size++;
+
+    for(i = 0; i<=size-1; i++){
+        printf("%d ",a[i]);
+    }
 }
