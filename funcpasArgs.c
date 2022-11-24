1aa73fe1b66098a680805191ddd4bdd36a318477
briankmbugua <briankmbuguak@gmail.com>
2022-11-24 22:41:00 +0300

    function pointers

--- funcpasArgs.c
+++ funcpasArgs.c
@@ -8,6 +8,19 @@ int main(){
     int num, width, i;
     num = sizeof(arr)/sizeof(arr[0]);
     width = sizeof(arr[0]);
-    printf("%d\n", num);
-    printf("%d\n", width);
+    printf("num of elements = %d\n", num);
+    printf("width of array = %d\n", width);
+    qsort((void *)arr, num, width, compare);
+    for(i = 0; i < 5; i++){
+        printf("%d\n", arr[i]);
+        
+    }
+    return 0;
+}
+
+int compare(const void *elem1, const void *elem2) {
+    if ((*(int *)elem1) == (*(int *)elem2))  return 0;
+    else if (((*(int *)elem1) == (*(int *)elem2))  ) return -1;
+    else return 1;
+    
 }
