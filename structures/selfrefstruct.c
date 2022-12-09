aa711ca7b1491ef0865db2485e73efbdcd98aa10
briankmbugua <briankmbuguak@gmail.com>
2022-12-10 01:42:31 +0300

    structures

--- structures/selfrefstruct.c
+++ structures/selfrefstruct.c
@@ -23,11 +23,11 @@ ptr_dog = &my_dog;//now ptr_dog points to the structure varaible my_dog
 
 //accessing members using pointers
 //using indirection pointer (*) and (.) operator
-//usng arrow(->)operator or membership operator
-
 (*ptr_dog).age = 10;
-
-
+//usng arrow(->)operator or membership operator
+ptr_dog->age = 20;
+(*ptr_dog).age = 10;
+printf("%d\n", ptr_dog->age);
 }
 
 
