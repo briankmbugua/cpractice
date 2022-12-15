9f7539e886d16a1817b93485da5d35f62d763476
briankmbugua <briankmbuguak@gmail.com>
2022-12-16 01:14:41 +0300

    linked list

--- linkedLists/linkedlist2.c
+++ linkedLists/linkedlist2.c
@@ -8,6 +8,7 @@ struct node
 };
 
 void count_of_node(struct node *head);
+void print_data(struct node *head);
 
 int main() {
     //struct node *head = NULL; //a pointer of type node
@@ -55,21 +56,46 @@ int main() {
     //to the third node
 
     count_of_node(head);
+    print_data(head); 
     return 0;
 } 
 
 
     //COUNTING NODES
     void count_of_node(struct node *head){
-        int count = 0;
-        if(head == NULL)
+        int count = 0;//declare a count variable initialized to 0
+        if(head == NULL){//head is the pointer pointing to the first node
+                        //if hed is NULL which means it is not pointing to anything
+                        //then the linked list is empty
         printf("linked list is empty");
+    }
 
-        struct node *ptr = NULL;
-        ptr = head;
-        while(ptr != NULL) {
+        struct node *ptr = NULL;//create a pointer of type struct node and initialize it to 
+                                //NULL
+        ptr = head;//put the value of head inside the ptr created this means ptr now
+                   //points to the first node of the list
+        while(ptr != NULL) {//this while loop will run untill ptr becomes NULL
             count++;
-            ptr = ptr->link;
+            ptr = ptr->link;//ptr->link is the memory address of the first node
+                            //after this code runs ptr now contains the memory address of the
+                            //first node which we can then use to access the link part of the
+                            //first node which gives us access to the third node and so on
+                            //untill ptr becomes NULL since the last node in a singly
+                            //linke list contains NULL in its link member
         }
         printf("\n%d", count);
     }
+
+void print_data(struct node *head){
+    if(head == NULL){
+        printf("The list is empty");
+    }
+    struct node *ptr = NULL;
+
+    ptr = head;
+
+    while(ptr != NULL){
+        printf("\nprinting from data from the linked list %d", ptr->data);
+        ptr = ptr->link;
+    }
+}
